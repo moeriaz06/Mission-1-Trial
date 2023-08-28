4bbcf9c9049a61dc2afea804a113753da93646a0
moeriaz06 <143030436+moeriaz06@users.noreply.github.com>
2023-08-28 03:14:23 +0000

    Trialing code in own repoistory

---
+++ trial.py
@@ -0,0 +1,12 @@
+import hello
+import pytest
+
+def trial_print():
+  print('Trialing the print function')
+  assert hello.print_hello('Hello World!') == Hello World!
+
+def test_add():
+  first_number = ('6')
+  second_number = ('6')
+  added_together = first_number + second_number
+  assert hello.add_values(first_number,second_number) == added_together
